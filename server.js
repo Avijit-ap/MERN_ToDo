c8c2675ef2083cb2415b726e62078e86921a2202
Avijit Pal <56922114+Avijit-ap@users.noreply.github.com>
2024-09-07 14:03:59 +0000

    changed file path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const todoRoutes = require('./routes/todoRoutes');
+const todoRoutes = require('./server/routes/todoRoutes');
 
 dotenv.config(); // Load environment variables from .env
 
